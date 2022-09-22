ecc5738a7c764c8db114dba124c971fc1ab94a72
Urban Dolenc <urban.dolenc9@gmail.com>
2022-09-22 11:15:17 +0200

    kalkulator

--- calculator/main.py
+++ calculator/main.py
@@ -1,4 +1,10 @@
+
 operacija = input("Izberi racunsko operacijo (+,-,/,*): ")
+
+while operacija != "+" and operacija != "-" and operacija != "/" and operacija != "*":
+    print("Napacen vnos operacije.")
+    operacija = input("Izberi racunsko operacijo (+,-,/,*): ")
+
 stevilo1 = input("Vnesite prvo stevilo: ")
 stevilo2 = input("Vnesite drugo stevilo: ")
 rezultat_str = "Rezultat operacije je: "
@@ -11,8 +17,5 @@ elif operacija == "/":
     rezultat = float(stevilo1) / float(stevilo2)
 elif operacija == "*":
     rezultat = float(stevilo1) * float(stevilo2)
-else:
-    rezultat_str = "Napačen vnos operacije."
-    rezultat = ""
 
 print(rezultat_str + str(rezultat))
